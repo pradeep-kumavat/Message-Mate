51bd0acf633bdaefd0fd6ef72b7aae33e518560a
pradeep-kumavat <pradeepkumavat108@gmail.com>
2024-06-16 18:01:24 +0530

    url updated

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
 	cors: {
-		origin: ["https://message-mate-z7wy.onrender.com/"],
+		origin: ["https://message-mate-z7wy.onrender.com"],
 		methods: ["GET", "POST"],
 	},
 });
